7fec38c47e633045dbb72c6ec7011772b84c6896
root <sungket_rhymes@hotmail.com>
2025-08-29 10:39:12 +0100

    added the contact.php boilerplate code from kala portfolio

--- forms/contact.php
+++ forms/contact.php
@@ -6,36 +6,59 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  ini_set('display_errors', 'Off');
+	error_reporting(E_ALL);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $subject = $_POST["subject"];
+  $message = $_POST["message"];
+
+  //Load Composer's autoloader
+  require "../vendor/autoload.php";
+
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\SMTP;
+  use PHPMailer\PHPMailer\Exception;
+
+  $mail = new PHPMailer(true);
+
+  try {
+    //server settings
+    $mail->SMTPDebug = 0;
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    // $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    $mail->Username = "sunj.rhymes";
+    $mail->Password = "veaz rkjd sevu ubwk";
+    $mail->SMTPOptions = array(
+      'ssl' => array(
+          'verify_peer' => false,
+          'verify_peer_name' => false,
+          'allow_self_signed' => true
+      )
+    );
+
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress("sunj.rhymes@googlemail.com");
+    $mail->addReplyTo($email);
+
+    //Content
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+
+    //Just need plain text message
+    $mail->isHTML(false);
+
+    $mail->send();
+
+    //echo "message sent! Thank you.";
+    //echo json_encode(['success' => true, 'message' => 'Message sent successfully.']);
+    echo "OK";
+  } catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
   }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
